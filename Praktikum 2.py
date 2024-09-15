d3b4436f73ef71638aa70f801c45726017315be2
Dapaa <123795852+Dapaaaaaa@users.noreply.github.com>
2024-09-15 18:43:54 +0800

    Add files via upload

---
+++ Praktikum 2.py
@@ -0,0 +1,22 @@
+nama = str(input("Masukan nama : "))
+umur = int(input("Masukan umur : "))
+jk_input = bool(input("Masukan jenis kelamin (0 = laki-laki, 1 = perempuan) : "))
+jk = bool(jk_input)
+gender = "laki-laki" if not jk else "perempuan"
+tb = float(input("Masukan tinggi badan : "))
+bb = float(input("Masukan berat badan : "))
+
+width = 50
+
+print("=" * width)
+print("Bio Data Anda".center(width))
+print("=" * width)
+
+print(f"Nama                    : {nama}")
+print(f"Umur                    : {umur} tahun")
+print(f"Jenis Kelamin           : {gender}")
+print(f"Tinggi Badan            : {tb} cm")
+print(f"Berat Badan             : {bb} kg")
+print(f"total integer dan float : {umur+tb+bb:.2f}")
+
+print("=" * width)  
