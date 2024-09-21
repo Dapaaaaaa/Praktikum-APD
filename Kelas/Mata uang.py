e23ce754a3ed9880f39d4caec33ed9ceb89e1c0f
Dapaa <123795852+Dapaaaaaa@users.noreply.github.com>
2024-09-21 14:53:11 +0800

    Delete Kelas directory

--- Kelas/Mata uang.py
+++
@@ -1,8 +0,0 @@
-USD = 0.000065
-EUR = 0.00006
-JPY = 0.0092
-
-IDR = float(input("Masukkan nominal uang: "))
-
-# print("Rp", IDR, " = USD", USD * IDR, " = EUR", EUR * IDR, " = JPY", JPY * IDR)
-print("Rp", IDR, " = USD", round(USD * IDR, 2), " = EUR", round(EUR * IDR, 2), " = JPY", round(JPY * IDR, 2))
